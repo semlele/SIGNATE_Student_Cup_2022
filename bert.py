a61485ea52ebc32c52621973f8298cc9f168fcc8
semlele <ken99suzu@icloud.com>
2022-08-04 00:01:32 +0900

    パラメータの詳細記載

--- bert.py
+++ bert.py
@@ -42,9 +42,10 @@ DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 TRAIN_FILE = os.path.join(data_dir, "train.csv")
 TEST_FILE = os.path.join(data_dir, "test.csv")
 MODELS_DIR = "./models/"
-MODEL_NAME = 'bert-base-uncased'
-TRAIN_BATCH_SIZE = 32
+MODEL_NAME = 'bert-base-uncased' #使用する自然言語モデルを選択。今回は、BERTのモデルでよく使用されているbert-base-uncasedを使用。Hugging Faceのサイトから好きなモデルを選ぶことも可能。（モデルよって出力などが異なるので、プログラム修正が必要）
+TRAIN_BATCH_SIZE = 32 #学習時に最適なパラメータを探索するときに、データを小さく分割して探索を行う。（学習用・検証用とかのデータ分割ではない）この時の、データサイズ。機械学習の慣例として、2^nで決める。
 VALID_BATCH_SIZE = 128
 NUM_CLASSES = 4
-EPOCHS = 5
-NUM_SPLITS = 5
+EPOCHS = 5 #学習する回数。1回で十分に学習できることは、ほとんどないので5回～10回など学習させる。
+NUM_SPLITS = 5 #K分割交差検証の分割数を決める。データ数により変わりますが、5個で分割するのが一般的。
+
